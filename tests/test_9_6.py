645e6569154409649c831e6ace61f491f4a438a8
akraeva <adkraeva@gmail.com>
2026-01-21 04:09:17 +0300

    Add Module 9 Chapter 6 solutions

--- tests/test_9_6.py
+++ tests/test_9_6.py
@@ -35,11 +35,11 @@ def test_9_6_1(input_data, expected, mocker, capsys):
     [
         (
             "imagine dragons metallica\nmetallica queen pink",
-            "common: metallica\nvasya: dragons imagine\npetya: pink queen",
+            "['metallica']\n['dragons', 'imagine']\n['pink', 'queen']",
         ),
-        ("a b c\nd e f", "common: -\nvasya: a b c\npetya: d e f"),
-        ("rock\nrock metal", "common: rock\nvasya: -\npetya: metal"),
-        ("abc\nabc", "common: a b c\nvasya: -\npetya: -"),
+        ("a b c\nd e f", "[]\n['a', 'b', 'c']\n['d', 'e', 'f']"),
+        ("rock\nrock metal", "['rock']\n[]\n['metal']"),
+        ("a b c\na b c", "['a', 'b', 'c']\n[]\n[]"),
     ],
 )
 def test_9_6_2(input_data, expected, mocker, capsys):
@@ -52,7 +52,11 @@ def test_9_6_2(input_data, expected, mocker, capsys):
 @pytest.mark.parametrize(
     "input_data, expected",
     [
-        ("Съешь же ещё этих мягких французских булок", "True"),
+        (
+            "Аэрофотосъёмка ландшафта уже выявила земли богачей и процветающих крестьян.",
+            "True",
+        ),
+        ("Съешь же ещё этих мягких французских булок, да выпей чаю", "True"),
         ("привет мир", "False"),
         ("абвгдеёжзийклмнопрстуфхцчшщъыьэюя", "True"),
         ("а б в", "False"),
@@ -85,12 +89,20 @@ def test_9_6_4(input_data, expected, mocker, capsys):
     "input_data, expected",
     [
         (
-            "ivan petya\npetya katya",
-            "both: petya\ninform: ivan\nphysic: katya\none: ivan katya petya",
+            "Arthur Peter Anna Alice Anastasia"
+            "\nAlice Arthur Vasily Anastasia Anna Tatiana Victoria Vadim",
+            "Alice, Anastasia, Anna, Arthur\n"
+            "Peter\n"
+            "Tatiana, Vadim, Vasily, Victoria\n"
+            "Peter, Tatiana, Vadim, Vasily, Victoria",
         ),
-        ("a b c\nd e", "both: -\ninform: a b c\nphysic: d e\none: a b c d e"),
-        ("x\nx y z", "both: x\ninform: -\nphysic: y z\none: x y z"),
-        ("1\n2", "both: -\ninform: 1\nphysic: 2\none: 1 2"),
+        (
+            "ivan petya\npetya katya",
+            "petya\nivan\nkatya\nivan, katya",
+        ),
+        ("a b c\nd e", "-\na, b, c\nd, e\na, b, c, d, e"),
+        ("x\nx y z", "x\n-\ny, z\ny, z"),
+        ("1\n2", "-\n1\n2\n1, 2"),
     ],
 )
 def test_9_6_5(input_data, expected, mocker, capsys):
